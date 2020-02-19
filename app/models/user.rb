d031029e3213bbfd4650f2f74a9ff332fdeeccc3
github username <github email address>
2020-02-19 07:00:49 -0500

    sets up devise in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable,
+        :jwt_authenticatable,
+        :registerable, 
+        jwt_revocation_strategy: JwtBlacklist
+
  
   has_many :user_albums
   has_many :albums, through: :user_albums
