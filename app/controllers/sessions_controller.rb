ef6c41267319c48dee76947fbc14ae02228371ae
github username <github email address>
2020-02-19 22:16:13 -0500

    overrides controllers to get json responses

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < Devise::SessionsController
+
+   respond_to :json
+
+   def respond_with(resource, _opts = {})
+      render json: resource
+   end
+
+
+   def respond_to_on_destroy 
+      head :no_content
+   end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  # sets routes for login, logout + signup
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
   
 end
