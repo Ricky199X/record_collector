221e655f737e26c68c651a4c917a86c2e53860ba
github username <github email address>
2020-02-21 21:53:07 -0500

    routes set up

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+   def index
+      render :json { message: 'welcome home'}
+   end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  resources :user_albums
-  resources :songs
-  resources :albums
-  resources :artists
+  # resources :user_albums
+
+  resources :albums do 
+    resources :songs
+  end
+
+  resources :artists do 
+    resources :albums
+  end
+
+  resources :users do 
+    resources :albums, only: [:index, :show]
+  end
+
   # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
