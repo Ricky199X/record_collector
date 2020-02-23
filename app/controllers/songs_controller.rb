3cf3957fe0a7607b4868a455c7e8a6b028efce91
github username <github email address>
2020-02-22 22:51:23 -0500

    songs controller set up

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,16 @@
 class SongsController < ApplicationController
+
+   before_action :authenticate_user!
+
+   def index
+      albums = current_user.albums
+      songs = albums.songs
+      render_resource(songs)
+   end
+
+   def show
+      song = Song.find(params[:id])
+      render_resource(song)
+   end
+   
 end
