89ef2844da498ba6fb3ee38799a27db0ad32571f
github username <github email address>
2020-02-29 14:25:46 -0500

    adds create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
       render json: UserSerializer.new(users)
    end
 
+   def create
+      user = User.new(user_params)
+      if user.save
+         render json: UserSerializer.new(user)
+      else 
+         render json: {error: 'Error creating user!'}
+      end
+   end
+
    def show
       user = User.find(params[:id])
       render json: UserSerializer.new(user)
