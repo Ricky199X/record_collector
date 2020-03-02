5fb8eb5f46277b2be05455b55ea40c4ea9163d7f
github username <github email address>
2020-03-02 18:19:51 -0500

    adds create method to sessions corntroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,22 @@
 class SessionsController < Devise::SessionsController
-    respond_to :json
+   respond_to :json
 
 
-    def new
-    end
+   def new
+   end
 
-    # def create
-    #     user = User.find_by(email: params[:email])
-        
+   def create
+      user = User.find_by(email: params[:email])
+      if !user.nil && user.authenticate(params[:password])
+         log_in(user)
+         render json: { message: 'welcome back!'}
+      else 
+         render json: { message: 'Improper credentials given!'}
+         # need to figure out what to do about the redirect here
+      end
+   end
 
-
-    # end
-
-    private
+   private
 
 
 end
