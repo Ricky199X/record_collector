7c474f4ad287802fba8f6a356b5395d5500d866f
github username <github email address>
2020-03-02 18:12:13 -0500

    removes JWT routes previously set up

--- config/routes.rb
+++ config/routes.rb
@@ -9,26 +9,13 @@ Rails.application.routes.draw do
     resources :albums
   end
 
-  # resources :users do 
-  #   resources :albums, only: [:index, :show]
-  # end
-
-  # devise_for :users
+  resources :users do 
+    resources :albums, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
 
-  # sets routes for login, logout + signup
-  # devise_for :users,
-  #     path: '',
-  #     path_names: {
-  #       sign_in: 'login',
-  #       sign_out: 'logout',
-  #       registration: 'signup'
-  #     },
-  #     controllers: {
-  #       sessions: 'sessions',
-  #       registrations: 'registrations'
-  #     }
+
   
 end
