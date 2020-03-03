541ddeb6595c9662e7784523712b7f3bf0cc23ee
github username <github email address>
2020-03-02 22:35:35 -0500

    adds logged_in + session destory actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
 
+    # command prevents Rails from using its authenticity token. 
+    # This is a security token that Rails generates from our session data and adds to the parameters sent 
+    # from a Rails form to a controller action 
+    # to prevent cross-site request forgery (CSRF) attacks.
+    
+    skip_before_action :verify_authenticity_token
     helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
 
 
@@ -16,7 +22,7 @@ class ApplicationController < ActionController::Base
     end
 
     def authorized_user?
-        @user == current_user
+        user == current_user
     end
 
     def logout!
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,40 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:email])
       if !user.nil && user.authenticate(params[:password])
          login!
-         render json: { message: 'welcome back!'}
+         render json: { 
+            logged_in: true,
+            user: user
+            message: 'welcome back!'
+         }
       else 
-         render json: { message: 'Improper credentials given!'}
+         render json: { 
+            status: 401,
+            message: 'Improper credentials given!'
+         }
          # need to figure out what to do about the redirect here
       end
+   end
+
+   def is_logged_in?
+      if logged_in? && current_user
+        render json: {
+          logged_in: true,
+          user: current_user
+        }
+      else
+        render json: {
+          logged_in: false,
+          message: 'no such user'
+        }
+      end
+   end
+
+   def destroy
+      logout!
+      render json: {
+        status: 200,
+        logged_out: true
+      }
    end
 
    private
