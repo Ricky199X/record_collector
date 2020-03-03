f9e569fd66853e6a820e3c24933079c959b250c8
github username <github email address>
2020-03-02 22:41:22 -0500

    adds routes for session controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  # routes for session controller actions
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+
 
   
 end
