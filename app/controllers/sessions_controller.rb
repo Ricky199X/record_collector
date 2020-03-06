1b400a10b4751d5f9c2a54c9f536ad0cf0ae3d32
github username <github email address>
2020-03-05 21:00:51 -0500

    action create could not b found SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 
    def create
       @user = User.find_by(email: params[:email])
+      binding.pry
       if !@user.nil && @user.authenticate(params[:password])
          session[:user_id] = @user.id
          cookies["logged_in"] = true
