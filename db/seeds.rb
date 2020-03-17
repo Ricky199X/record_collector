16f170045c819b61111f9338beee44baa485c909
github username <github email address>
2020-03-17 17:50:39 -0400

    adds additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create(username: 'username', email: 'ricky@ricky.com', password: 'password')
+user = User.create(username: 'user2', email: 'user2', password: 'user2')
 
 artist = Artist.create(name: 'OutKast', genre: 'Rap', photo_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/OutKast_2001.jpg/800px-OutKast_2001.jpg')
+artist = Artist.create(name: 'Kendrick Lamar', genre: 'Rap', photo_url: 'https://media.gq.com/photos/5e6151090f0a6a0008f45908/master/w_3000,h_1873,c_limit/GQ_KendrickLamar_030520.jpg')
 
 album = Album.create(name: 'Aquemini', release_date: '1998-09-29', label: 'LaFace Records', cover_url: 'https://upload.wikimedia.org/wikipedia/en/2/2c/AqueminiOutKast.jpg', popularity: 90, artist_id: 1)
+album = Album.create(name: 'To Pimp a Butterfly', release_date: '2015-03-15', label: 'Interscope Records', cover_url: 'https://upload.wikimedia.org/wikipedia/en/f/f6/Kendrick_Lamar_-_To_Pimp_a_Butterfly.png', popularity: 100, artist_id: 2)
 
 song = Song.create(name: 'Aquemini', duration_ms: 100000, preview_url: 'www.preview.com', album_id: 1, artist_id: 1)
+song = Song.create(name: 'Alright', duration_ms: 100000, preview_url: 'www.preview.com', album_id: 2, artist_id: 2)
 
 user_album = UserAlbum.create(user_id: 1, album_id: 1, artist_id: 1)
